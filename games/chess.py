10e924b8c1665180e8210950ff189d65ad9f0638
Zachary Marion <zac.marion@coinbase.com>
2018-07-18 23:50:44 -0700

    Basic outline

---
+++ games/chess.py
@@ -0,0 +1,20 @@
+from core.game import Game
+
+class TicTacToe(Game):
+  def initial_state(self):
+    pass
+
+  def action_space(self, s):
+    pass
+
+  def terminal(self, s):
+    pass
+
+  def reward(self, s, p):
+    pass
+  
+  def next_state(self, s, a, p):
+    pass
+
+  def to_string(self, s):
+    pass
